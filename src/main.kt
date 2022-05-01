66ca6c95014a77a5f69b5ceaba046a21ee6788bb
Shop <mohamedrohiem597@gmail.com>
2022-05-01 10:05:03 +0200

    var and val

--- src/main.kt
+++ src/main.kt
@@ -1,5 +1,8 @@
 fun main() {
- println("hello world")
- println("hello world")
- println("hello world")
+ //var name="moahmed"
+ // name="sayed" i will get none error
+ //println(name.isEmpty())
+ val firstName="ali"
+ //firstName="mohamed   will get error it is constant
+ println(firstName)
 }
